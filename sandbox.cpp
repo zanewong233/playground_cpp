08d6f565c5fd7c198ef6e79f6ce5d1009400488c
JhonWong <1290773033@qq.com>
2025-07-08 22:21:06 +0800

    std::list并行版实现

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <future>
 #include <list>
 #include <mutex>
 #include <numeric>
@@ -19,13 +20,13 @@ std::list<T> SequenceQuickSort(std::list<T>&& input) {
     return input;
   }
 
-  T diviot = std::move(*input.begin());
+  T pivot = std::move(*input.begin());
   input.pop_front();
 
   std::list<T> lower, higher;
   for (auto it = input.begin(); it != input.end();) {
     auto cur = it++;
-    if (*cur < diviot) {
+    if (*cur < pivot) {
       lower.splice(lower.end(), input, cur);
     } else {
       higher.splice(higher.end(), input, cur);
@@ -35,14 +36,41 @@ std::list<T> SequenceQuickSort(std::list<T>&& input) {
   lower = SequenceQuickSort(std::move(lower));
   higher = SequenceQuickSort(std::move(higher));
 
-  lower.push_back(std::move(diviot));
+  lower.push_back(std::move(pivot));
+  lower.splice(lower.end(), higher);
+  return lower;
+}
+
+template <typename T>
+std::list<T> ParallelQuickSort(std::list<T>&& input) {
+  if (input.size() <= 1) {
+    return input;
+  }
+  T pivot = std::move(*input.begin());
+  input.pop_front();
+
+  std::list<T> lower, higher;
+  for (auto it = input.begin(); it != input.end();) {
+    auto cur = it++;
+    if (*cur < pivot) {
+      lower.splice(lower.end(), input, cur);
+    } else {
+      higher.splice(higher.end(), input, cur);
+    }
+  }
+
+  auto fut = std::async(&ParallelQuickSort<T>, std::move(lower));
+  higher = ParallelQuickSort(std::move(higher));
+
+  lower = fut.get();
+  lower.push_back(std::move(pivot));
   lower.splice(lower.end(), higher);
   return lower;
 }
 
 int main() {
   std::list<int> list{5, 6, 7, 1, 4, 54, 48};
-  auto sorted = SequenceQuickSort(std::move(list));
+  auto sorted = ParallelQuickSort(std::move(list));
 
   int a = 10;
   a++;
