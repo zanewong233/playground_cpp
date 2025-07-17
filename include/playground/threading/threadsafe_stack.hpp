535bdfd243d1c6f0e5c5dbac6a20461c74f747c3
johnwongx <1290773033@qq.com>
2025-07-17 14:44:42 +0800

    并行sort的简单实现

--- include/playground/threading/threadsafe_stack.hpp
+++ include/playground/threading/threadsafe_stack.hpp
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef PLAYGROUND_THREADING_THREADSAFE_STACK_H_
+#define PLAYGROUND_THREADING_THREADSAFE_STACK_H_
 #include <mutex>
 #include <numeric>
 #include <stack>
@@ -6,15 +7,8 @@
 #include <vector>
 
 namespace playground {
-struct EmptyStack : public std::exception {
-  const char* what() const noexcept override { return "Empty stack!"; }
-};
-
 template <typename T>
 class ThreadsafeStack {
-  std::stack<T> data_;
-  std::mutex m_;
-
  public:
   ThreadsafeStack() : data_(std::stack<T>()) {}
   ThreadsafeStack(const ThreadsafeStack& other) {
@@ -23,32 +17,46 @@ class ThreadsafeStack {
   }
   ThreadsafeStack& operator=(const ThreadsafeStack&) = delete;
 
-  void Push(const T& new_value) {
+  void Push(T new_value) {
     std::lock_guard lock(m_);
-    data_.push(new_value);
+    data_.push(std::move(new_value));
   }
 
-  void Pop(T& value) {
+  bool Pop(T& value) {
     T tmp;
     {
       std::lock_guard lock(m_);
-      if (data_.empty()) throw EmptyStack{};
+      if (data_.empty()) {
+        return false;
+      }
       tmp = std::move(data_.top());
       data_.pop();
     }
     value = std::move(value);
+    return true;
   }
 
   std::shared_ptr<T> Pop() {
     T tmp;
     {
       std::lock_guard lock(m_);
-      if (data_.empty()) throw EmptyStack{};
+      if (data_.empty()) {
+        return {};
+      }
       tmp = std::move(data_.top());
       data_.pop();
     }
     return std::make_shared<T>(std::move(tmp));
   }
-};
 
+  size_t Size() const {
+    std::lock_guard lock(m_);
+    return data_.size();
+  }
+
+ private:
+  std::stack<T> data_;
+  mutable std::mutex m_;
+};
 }  // namespace playground
+#endif
